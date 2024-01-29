29b58f940c80b6fe7d7c0d2f57aa65c8998c2e5a
transcenddev <97653516+r3kashi@users.noreply.github.com>
2024-01-29 12:57:48 +0800

    Added modern css reset

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,6 +12,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../styles/dashboard.css">
+    <link rel="stylesheet" href="../styles/main.css">
     <?php include('../config/config.php'); ?>
 
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
